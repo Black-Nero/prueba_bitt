e510cad8cf919f41c98f0fbb5e94cd7aec9774bb
Black-Nero <gabosantiago76@gmail.com>
2023-03-04 18:31:52 -0600

    sabiduria de ale

--- ale.py
+++ ale.py
@@ -1,1 +1,3 @@
 print ("Ale dice : HOLAAAAAAAAAA")
+print ("Ale sabe cosas, y te las puede decir si le das, pan!!")
+print ("pan y lechita, son necesarios para que ale diga un cacho de su sabiduria")
